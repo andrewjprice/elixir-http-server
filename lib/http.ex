b8a82a993bda12fa81d204d8b78429fb29dca37a
Andrew <andrew.j.price@outlook.com>
2020-02-17 20:00:52 -0800

    Setup basic echo server to read and write back data

--- lib/http.ex
+++ lib/http.ex
@@ -1,18 +1,35 @@
 defmodule Http do
-  @moduledoc """
-  Documentation for `Http`.
-  """
+  require Logger
 
-  @doc """
-  Hello world.
+  # listen to port until connection
+  def start_link(port) do
+    {:ok, socket} = :gen_tcp.listen(port, [:binary, packet: :line, active: false, reuseaddr: true])
+    Logger.info("Accepting connections on port: #{port}")
+    # {:ok, spawn_link(Http, :accept, [socket])}
+    accept(socket)
+  end
 
-  ## Examples
+  # handle connections
+  defp accept(socket) do
+    {:ok, request} = :gen_tcp.accept(socket)
+    serve(request)
+    accept(socket)
+  end
 
-      iex> Http.hello()
-      :world
+  # receive and send data
+  defp serve(socket) do
+    socket
+    |> readline()
+    |> writeline(socket)
+    serve(socket)
+  end
 
-  """
-  def hello do
-    :world
+  defp readline(socket) do
+    {:ok, data} = :gen_tcp.recv(socket, 0)
+    data
+  end
+
+  defp writeline(line, socket) do
+    :gen_tcp.send(socket, line)
   end
 end
